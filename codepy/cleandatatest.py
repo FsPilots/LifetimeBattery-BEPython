0b5bed455750bb071b0453170130868ced08dd03
FsPilots <Alexandregasse.hellio@gmail.com>
2024-02-27 17:33:24 +0100

    Update cleandata

--- codepy/cleandatatest.py
+++ codepy/cleandatatest.py
@@ -84,12 +84,12 @@ for index, ligne in dataframe.iterrows():
         #ChargingTime
         CT_sup=dataframe.loc[index+1, 'Charging time (s)']
         #RUL
-        RUL_sup=dataframe.loc[index+1, 'RUL']
-        
-        
-        
+        RUL_sup=dataframe.loc[index+1, 'RUL']  
         
     #Déclaration des variables des vérifs
+    #Vérif Decrement 3.6-3.4V
+    Up64=1300
+    Down64=100
     #Vérif Charging time
     MarginPhyCT=25/100
     FunctionCTrule=10000*(4.3-Min_act)
@@ -100,7 +100,6 @@ for index, ligne in dataframe.iterrows():
     #Vérif Decharging time
     MarginPhyDT=25/100
     FunctionDTrule=10000*(4.3-Min_act)
-    #Vérif Decrement 3.6-3.4V
         
     #Init Variables de modifications
     New_CI=CI_act
@@ -117,39 +116,59 @@ for index, ligne in dataframe.iterrows():
     if(CI_act > CI_ant):
         if(RUL_act > RUL_ant):
             New_RUL=1113-CI_act
+            
+    #Vérif Decrement 3.6-3.4V
+    if(Up64>Dec64_act>Down64):
+        #Validation Dec64
+        New_Dec64=Dec64_act
+    else:
+        #Correction Dec64
+        New_Dec64=(Dec64_ant+Dec64_sup)/2
+        Dec64_act=New_Dec64
     
     #Vérif Charging time - On pose A = RUL * (4.3 - Min voltage) = MarginPhy - On peut supposer que 10% peut être modifier
     #Si A + 10%*A > Charging time > A - 10%*A OK sinon Charging time n = A
     #On veut pouvoir faire évoluer LinearCTrule pour être de plus en plus précis dans la correction des données
     if(FunctionCTrule*(1+MarginPhyCT)>CT_act>FunctionCTrule*(1-MarginPhyCT)):
+        #Validation CT
         New_CT=CT_act
     else:
-        New_CT=round(FunctionCTrule,1)
+        #Correction CT
+        New_CT=FunctionCTrule
+        CT_act=New_CT
     
     #Vérif Time constant current and Time 4.15V 
     #La vérification concerne la validité de la corrélation entre le temps à 4.15V et le temps avec courant constant.
     #Si ls valeurs sont incompatibles, elles sont remplacées en fonction des paramètres inférieurs et suppérieurs.
-    if(Time415_act+1000+1000*MarginPhyT415>TCC_act>Time415_act+1000-1000*MarginPhyT415 and TCC_act<10000):
+    if(Time415_act+Dec64_act+Dec64_act*MarginPhyT415>TCC_act>Time415_act+Dec64_act-Dec64_act*MarginPhyT415 and TCC_act<10000):
+        #Validation TCC et Time415
         New_TCC=TCC_act
         New_Time415=Time415_act
     else:
+        #Correction TCC
         New_TCC=(TCC_ant+TCC_sup)/2
         TCC_act=New_TCC
         if(TCC_act-1000+1000*MarginPhyTCC>Time415_act>TCC_act-1000-1000*MarginPhyTCC):
             New_Time415=Time415_act
         else:
+            #Correction Time415
             New_Time415=(Time415_sup+Time415_ant)/2
             Time415_act=New_Time415
             
-    #Vérif Decrement 3.6-3.4V
-    
     #Vérif Discharge time - De même que pour la Vérif Charging time, cette vérif a pour but de supprimer les valeurs abérantes
     #en suivant une loi idéale qui peut être mise a jour par le modèle - FunctionDTrule
     #Dépend du Dec64 et Time415
     
+    if(CT_act>DT_act and DT_act>800):
+        #Validation DT
+        New_DT=DT_act
+    else:
+        #Correction DT
+        New_DT=(DT_ant+DT_sup)/2
+        
+    
     if(index==0):
         print('Ligne',index+1,': Cycle_Index|Discharge Time (s)|Decrement 3.6-3.4V (s)|Max. Voltage Dischar. (V)|Min. Voltage Charg. (V)|Time at 4.15V (s)|Time constant current (s)|Charging time (s)|RUL')
-    
     print('Ligne',index+2,': CI:',New_CI,' DT:',New_DT,' Dec64:',New_Dec64,' Max:',New_Max,' Min:',New_Min,' Time415:',New_Time415,' TCC:',New_TCC,' CT:',New_CT,' RUL:',New_RUL)
     dataframe.loc[index, 'Cycle_Index'] = New_CI
     dataframe.loc[index, 'Discharge Time (s)'] = New_DT
