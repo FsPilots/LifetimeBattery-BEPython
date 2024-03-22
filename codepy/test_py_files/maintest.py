61c3eee4a86f5015b7266b2fbbd2493c6398e350
FsPilots <Alexandregasse.hellio@gmail.com>
2024-03-22 12:21:49 +0100

    Update

--- codepy/test_py_files/maintest.py
+++ codepy/test_py_files/maintest.py
@@ -11,27 +11,16 @@ dataframe = pd.read_csv('data/test_data/Battery_RUL_main_test.csv')
 #Définir un chemin de sortie des données clean
 output_clean_data_path='data/test_data/output_main/Battery_RUI_sortie_main_test.csv'
 
-<<<<<<< Updated upstream
 #Définir un chemin de sortie des données utiliables par l'IA
-=======
-#Définir un chemin de sortie des données utilisables
->>>>>>> Stashed changes
 output_usable_data_path='data/test_data/output_main/Battery_RUI_usable_data.csv'
 
 #Etape 1: Nettoyage des données
 #cd.clean_data(dataframe,output_clean_data_path)
 
 #Etape 2: Feature Engineering
-<<<<<<< Updated upstream
 #adt.analysedatatest(output_clean_data_path)
 fe.feature_eng(output_clean_data_path,output_usable_data_path)
 
 #Etape 3: Entrainement modèle IA et Analyse resultats
 RLmod.reglineaire(output_usable_data_path)
-=======
-fe.feature_eng(output_clean_data_path,output_usable_data_path)
-
-#Etape 3: Entrainement modèle IA et Analyse resultats
-#RLmod.reglineaire(output_usable_data_path)
->>>>>>> Stashed changes
 
